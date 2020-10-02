517f21957f73d6b8041d69a3b1646f8def9e7d08
didierganthier <didierganthierperan@gmail.com>
2020-10-02 17:45:06 -0400

    place_service.dart not working yet

--- lib/services/place_service.dart
+++ lib/services/place_service.dart
@@ -11,16 +11,17 @@ class PlacesService {
     return _service;
   }
 
-  final String searchUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=harbour&key=";
+  final String searchUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=harbour&key=AIzaSyDhjfDA7KRJ0mLUVG0mfdbQwXM_gZdYU3I";
 
   Future<List<Place>> getNearbyPlaces() async{
-    var response = await http.get(searchUrl, headers:{"Accept":"application/json"});
+
+    var response = await http.get(searchUrl, headers:{"Accept":"application/json"}).catchError((error) => print(error));
     var places = <Place>[];
 
     List data = json.decode(response.body)['results'];
 
     data.forEach((element) {
-      places.add(Place(icon: element['icon'], name: element['icon'], rating: element['rating'].toString(), vicinity: element['vicinity'], id: element['place_id']));
+      places.add(Place(icon: element['icon'], name: element['name'], rating: element['rating'].toString(), vicinity: element['vicinity'], id: element['place_id']));
     });
     return places;
   }
