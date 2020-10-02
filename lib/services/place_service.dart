c9299a85967827e0c6446135a22adb2c80c3e535
didierganthier <didierganthierperan@gmail.com>
2020-10-02 11:15:49 -0400

    place_service.dart implemented

---
+++ lib/services/place_service.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter_nearby_places/models/place_model.dart';
+import 'package:http/http.dart' as http;
+
+class PlacesService {
+  static final _service = PlacesService();
+
+  static PlacesService get(){
+    return _service;
+  }
+
+  final String searchUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=harbour&key=";
+
+  Future<List<Place>> getNearbyPlaces() async{
+    var response = await http.get(searchUrl, headers:{"Accept":"application/json"});
+    var places = <Place>[];
+
+    List data = json.decode(response.body)['results'];
+
+    data.forEach((element) {
+      places.add(Place(icon: element['icon'], name: element['icon'], rating: element['rating'].toString(), vicinity: element['vicinity'], id: element['place_id']));
+    });
+    return places;
+  }
+
+}
